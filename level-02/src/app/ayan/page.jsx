4a9db8975de8d5ac1b75ff3f34993d529b539497
ayan khan <ayankhanayan992@gmail.com>
2026-09-03 02:12:34 +0530

    routing in next.js

---
+++ level-02/src/app/ayan/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <h1>hii ayan</h1>
+    </div>
+  );
+};
+
+export default page;
---
+++ level-02/src/app/contact/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <h1>contact page is this</h1>
+    </div>
+  );
+};
+
+export default page;
