fba3d90f176a496269c728bc92cb4e2c1174b2f9
ayan khan <ayankhanayan992@gmail.com>
2026-09-06 20:19:00 +0530

    TS completed up to full basics

---
+++ level-04/src/Button.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+type buttonProps={
+    data :string,
+    action:()=>void
+}
+
+function Button({data,action}:buttonProps) {
+  return (
+    <div>
+      hii hlww
+    </div>
+  )
+}
+
+export default Button
+
---
+++ level-04/src/types.d.ts
@@ -0,0 +1,9 @@
+import { WithStringifiedURLs } from "next/dist/lib/metadata/types/metadata-interface"
+
+declare global {
+    type raja={
+name:string
+    }
+}
+
+export {}
